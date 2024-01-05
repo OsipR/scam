aa61658e47184321da2a2add07ef981a8b3e0c97
OsipR <153323342+OsipR@users.noreply.github.com>
2024-01-05 04:18:47 +0300

    guess the number

--- scam/src/App.js
+++ scam/src/App.js
@@ -6,8 +6,9 @@ function App() {
   const [random, setRandom] = useState();
   const [guess, setGuess] = useState('');
   const [attempt, setAttempt] = useState(5);
+  const [past, setPast] = useState([])
 
-console.log(attempt);
+///console.log(attempt);
   function randomNamber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -19,7 +20,9 @@ console.log(attempt);
   }, []);
   
   const check = () => {
-    if (guess == random) {
+    if (guess > 100) {
+      alert('вы ввели значение больше 100!');
+    } else if (guess == random) {
       alert('вы угадали, загаданное число: ' + random);
     } else if (guess < random) {
       setAttempt(attempt - 1);
@@ -28,23 +31,33 @@ console.log(attempt);
       setAttempt(attempt - 1);
       alert('загаданное число меньше');
     };
+    if (guess <= 100) {
+      const number = guess  + ' ';
+    setPast(oldArray => [...oldArray, number]);
+    }
   };
   
-
   
     
 
 
   return (
-    <div>
-      <div>{random}</div>
-      <div>Осталось попыток: {attempt}</div>
+    <div className= {styles.App}>
+      <h1 className={styles.h1}>ИГРА УГАДАЙ ЧИСЛО</h1>
+      <p className= {styles.p}>
+        <p>Правила игры:</p>
+        <p>1. Загаданно число от 1 до 100</p>
+        <p>2. У вас есть 5 попыток, чтобы угадать его</p>
+        </p>
+        
+      <div className= {styles.attempt}>Осталось попыток: {attempt}</div>
 
       <div>
-        <input type='text' value={guess} onChange={(e) => setGuess(e.target.value)}></input>
+        <input type='text' value={guess} onChange={(e) => setGuess(e.target.value)} className= {styles.guess}></input>
       </div>
-      <button onClick={() => check()}>проверить</button>
-      <button onClick={() => window.location.reload()}>заново</button>
+      <button onClick={() => check()} className={styles.buttonCheck}>проверить</button>
+      <button onClick={() => window.location.reload()} className= {styles.buttonReload}>заново</button>
+      <div className={styles.past}>Ваши прошлые варинаты ответа: {past}</div>
     </div>
   );
 }
