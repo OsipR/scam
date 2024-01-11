fd040d461f2c9d6902e0acdf09f2416552af30a7
OsipR <153323342+OsipR@users.noreply.github.com>
2024-01-11 23:30:36 +0300

    progress

--- scam/src/new/ser.js
+++ scam/src/new/ser.js
@@ -7,10 +7,27 @@ function Ser() {
         async function Arrau() {
             const response = await HttpNoData('/api/posts/2004/2024', 'GET');
             setPost(response)
-        } 
-Arrau();
-    }, []); 
-    return <div>{post.map(item => <div>{item.date}</div>)}</div>
+        }
+        Arrau();
+    }, []);
 
-};
+    //return <div>{post.map(item => <div>{item.date}</div>)}</div>
+
+    return (
+        <>
+            {post.map((item) => {
+                const { author, authorId, date, text, isBlock } = item
+                return (
+                    <div key={authorId}>
+                        <div>{author}</div>
+                        <div>{date}</div>
+                        <div>{text}</div>
+                        <div>{isBlock.toString()}</div>
+                    </div>
+                )
+            })}
+
+        </>
+    )
+}
 export default Ser;
