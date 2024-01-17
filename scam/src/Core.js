69ca771214560920b8f597a4bd38da859472279b
OsipR <153323342+OsipR@users.noreply.github.com>
2024-01-17 18:58:02 +0300

    Revert "Revert "unlucky""
    This reverts commit 9782995819a75241613aac815b39a870c8745f6b.

--- scam/src/Core.js
+++ scam/src/Core.js
@@ -13,8 +13,9 @@ export async function HttpNoData(url, method) {
 
     for (let i = 0; i < count; i++)
         result.push({
-            author: 'Николай' + i,
-            authorId: i + i,
+            id: i,
+            author: 'Николай' + (i % 2 === 0 ? i : 90),
+            authorId: (i % 2 === 0 ? i : 90),
             date: getRandomInt(2004,2024)+'-01-01T12:12:12.1457',
             text: 'This example returns a random integer between the specified values. The value is no lower than min (or the next integer greater than min if min isnt an integer), and is less than (but not equal to) max.',
             isBlock: i % 4 == 0
