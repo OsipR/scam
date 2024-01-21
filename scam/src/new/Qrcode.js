0d7ae45f1fc2d466363c728624f009ac77d46fbd
OsipR <153323342+OsipR@users.noreply.github.com>
2024-01-21 20:17:10 +0300

    qr finish!
    Co-Authored-By: Nikolay <90929111+Faclok@users.noreply.github.com>

--- scam/src/new/Qrcode.js
+++ scam/src/new/Qrcode.js
@@ -1,33 +1,35 @@
 import { useState, useEffect } from "react";
-import { Html5Qrcode } from "html5-qrcode";
+import { Html5Qrcode, Html5QrcodeScanType, Html5QrcodeScanner } from "html5-qrcode";
 
-function Qr () {
+function Qr() {
 
-const [scan, setScan] = useState(null);
+    const [scan, setScan] = useState(null);
 
-useEffect (() => {
-    const scaner = Html5Qrcode( 'reader', {
-        qrbox: {width: 250, height: 250}, fps: 10
-    });
-    
-    scaner.render(success, error);
-    
-    function success(result) {
-        scaner.clear();
-        setScan(result);
-    }
-    
-    function error(err) {
-        console.warn(err)
-    }
-},[]);
+    useEffect(() => {
+        function onScanSuccess(decodedText, decodedResult) {
+            // handle the scanned code as you like, for example:
+            console.log(`Code matched = ${decodedText}`, decodedResult);
+          }
+          
+          let config = {
+            fps: 10,
+            qrbox: {width: 512, height: 512},
+            rememberLastUsedCamera: true,
+            // Only support camera scan type.
+            supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+          };
+          
+          let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", config, /* verbose= */ false);
+          html5QrcodeScanner.render(onScanSuccess);
+    }, []);
 
 
-return (
-<div className = 'Qrcode'>
-    <h1>Qr Scaner!</h1>
-    { scan ? <div>success: {scan}</div> : <div id = 'reader'></div>}
-</div>
-);
+    return (
+        <div className='Qrcode'>
+            <h1>Qr Scaner!</h1>
+            {scan ? <div>success: {scan}</div> : <div id='reader'></div>}
+        </div>
+    );
 };
 export default Qr;
