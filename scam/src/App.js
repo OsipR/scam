74c2adcacab1f1b585845de22828c346fa34e5fd
OsipR <153323342+OsipR@users.noreply.github.com>
2024-01-29 12:51:47 +0300

    hz

--- scam/src/App.js
+++ scam/src/App.js
@@ -1,21 +1,35 @@
+import { useState } from 'react';
 import Ser from './new/Ser.js';
 import QrRead from './Qrcode/Qrcode.js';
 import Gen from './Qrcode/Qrcodegen.js';
+import styles from './App.module.css';
+import Gor from './goroscop/Goroscop.js';
 
 
 function App() {
 
-  const userData = {
-    userName: "Vova",
-    userId: 1,
-    code: 12342,
-  };
+  const [code, setCode] = useState();
+  const [codeScanner, setCodeScanner] = useState(111);
+  const [userId, setUserId] = useState(123);
 
+  const successCode = (data) =>{
+
+    if(data.userId !== userId)
+      return false;
+
+    setCode(data.code);
+    console.log(data.code);
+
+    return true;
+  } 
+
+  if(code)
+    return <div>{code}</div>;
   
-
   return (
-    <div>
-      <QrRead />
+    <div className={styles.App}>
+      <Gen code={codeScanner} userId={123} name={"VOVOV"}/>
+      <QrRead successCallback={successCode} cancelCallback={() => window.location.href = 'https:\\google.com'} updateCallback={() => setCodeScanner(codeScanner + 1)}/>
     </div>
     );
   // return (
--- scam/src/Qrcode/Qrcode.js
+++ scam/src/Qrcode/Qrcode.js
@@ -1,33 +1,72 @@
 import { useState, useEffect } from "react";
 import { Html5Qrcode, Html5QrcodeScanType, Html5QrcodeScanner } from "html5-qrcode"
 
-function QrRead() {
+function QrRead(props) {
 
-  const [scan, setScan] = useState(null);
+  const { successCallback, cancelCallback, updateCallback } = props;
 
-  useEffect(() => {
-    function onScanSuccess(decodedText, decodedResult) {
-      console.log(`Code matched = ${decodedText}`, decodedResult);
-    }
+  const [scan, setScan] = useState();
+  const [isField, setIsField] = useState(false);
 
-    let config = {
-      fps: 10,
-      qrbox: { width: 400, height: 400 },
-      rememberLastUsedCamera: true,
-      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
-    };
+  const BarcodeScannerPluginRework = () => {
+    let html5QrCode;
+    const qrcodeId = 'reader';
+    
+    useEffect(() => {
+        // Anything in here is fired on component mount.
+        if(!html5QrCode?.getState()){
+            html5QrCode = new Html5Qrcode(qrcodeId);
+            const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+              
+              if (successCallback(JSON.parse(decodedText))){
+                html5QrCode.pause();
+                html5QrCode.clear();
+              }
+              else console.log('lox');
+            };
+            const config = { fps: 10, qrbox: { width: 250, height: 250 }, aspectRatio: 1.777778};
 
-    let html5QrcodeScanner = new Html5QrcodeScanner(
-      "reader", config, false);
-    html5QrcodeScanner.render(onScanSuccess);
-  }, []);
+            // If you want to prefer back camera
+            html5QrCode.start(
+                { facingMode: "environment" },
+                config,
+                qrCodeSuccessCallback
+            );
+        }
 
+        return () => {
+            // Anything in here is fired on component unmount.
 
+        };
+    }, []);
+
+    return <div id={qrcodeId}></div>;
+};
+
+const Field = () => {
+
+  const [text, setText] = useState('');
+
+  return <div>
+    <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+    <button onClick={() => successCallback({ userId: 123, code: text })}>send</button>
+  </div>
+  
+}
 
   return (
     <div className='Qrcode'>
       <h1>Qr Scaner!</h1>
-      {scan ? <div>success: {scan}</div> : <div id='reader'></div>}
+      {
+        isField ?
+        <Field/>:
+      <BarcodeScannerPluginRework/>
+      }
+      <div>
+        <button onClick={cancelCallback}>cancel</button>
+        <button onClick={updateCallback}>update</button>
+        <button onClick={() => setIsField(!isField)}>input field</button>
+      </div>
     </div>
   );
 };
