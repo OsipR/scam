cb71d788a48c6764d706ebf9378cf8419e11bc31
OsipR <153323342+OsipR@users.noreply.github.com>
2024-02-09 00:11:42 +0300

    wrote shit that works better

--- scam/src/dayNight/DayNight.js
+++ scam/src/dayNight/DayNight.js
@@ -1,53 +1,61 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from './dayNight.module.css'
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import {motion} from 'framer-motion';
 
 function Dn() {
     const [showSun, setShowSun] = useState(true);
-    const [showMoon, setShowMoon] = useState(false);
+    const [showMoon, setShowMoon] = useState(true);
     const [show, setShow] = useState(true)
+    const nodeRef = useRef(null)
 
     var scroll =  window.addEventListener('scroll', function () {
         const scrollPosition = document.documentElement.scrollTop;
         // console.log(scrollPosition);
 
-        if (scrollPosition > 500) {
-            setShow(false);
-        } else {
+        if (scrollPosition < 500) {
             setShow(true)
         }
+        if (scrollPosition > 500) {
+            setShow(false);
+        } 
     
-    
+    // console.log(show)
 
     });
 
-    
+    const sunAnimation = {
+        hidden: {
+            x: -100,
+            opacity: 0,
+        },
+        visible: {  
+            x: 0,
+            opacity: 1,
+        }
+    };
+
+    const moonAnimation = {
+        hiddenMoon: {
+            x:-100,
+            opacity: 0,
+        },
+        visibleMoon: {
+            x: 0,
+            opacity: 1,
+        }
+        }
 
     return (
 
         <div>
             
+
+
 {/* <TransitionGroup>
-            <CSSTransition in={show} timeout={300} className={styles.Show} unmountOnExit>
-                <div>
-                {show ?
-                    <img className={styles.Sun} src="https://img.freepik.com/premium-photo/sun-cartoon-watercolor-childrens-illustration-of-the-sun-drawn-by-hand-isolated-on-a-white_276714-533.jpg?w=740" alt="" />
-                    : <img className={styles.Moon} src="https://hi-news.ru/wp-content/uploads/2013/08/wallpaper-111288-750x469.jpg" alt="" />}
-                    </div>
-            </CSSTransition>
-            </TransitionGroup>  */}
-
-
-             {/* <CSSTransition in={true} timeout={300} classNames={styles.Show} unmountOnExit></CSSTransition>
-            {showMoon? 
-            <img className={styles.Moon} src="https://hi-news.ru/wp-content/uploads/2013/08/wallpaper-111288-750x469.jpg" alt=""/>
-            : null}
-            </CSSTransition> */}
-
-<TransitionGroup>
         {show? 
-        <CSSTransition in={show} timeout={200} className={styles.Show} unmountOnExit>
-            <div>
+        <CSSTransition nodeRef={nodeRef} in={show} timeout={200} classNames="my-node" unmountOnExit>
+            <div ref={nodeRef}>
             <img className={styles.Sun} src="https://img.freepik.com/premium-photo/sun-cartoon-watercolor-childrens-illustration-of-the-sun-drawn-by-hand-isolated-on-a-white_276714-533.jpg?w=740" alt="" />
             </div>
         </CSSTransition>
@@ -59,16 +67,34 @@ function Dn() {
 
     <TransitionGroup>
         {show? null :
-        <CSSTransition in={show} timeout={200} className={styles.Show} unmountOnExit>
-            <div>
+        <CSSTransition nodeRef={nodeRef} in={show} timeout={200} classNames="my-node" unmountOnExit>
+            <div ref={nodeRef}>
             <img className={styles.Moon} src="https://hi-news.ru/wp-content/uploads/2013/08/wallpaper-111288-750x469.jpg" alt=""/>
             </div>
         </CSSTransition>
         }
-    </TransitionGroup>
+    </TransitionGroup> */}
+<motion.section
+initial="hidden"
+whileInView="visible"
+>
 
-
-
+        {show? 
+    <motion.div>
+            <motion.img variants={sunAnimation} className={styles.Sun} src="https://img.freepik.com/premium-photo/sun-cartoon-watercolor-childrens-illustration-of-the-sun-drawn-by-hand-isolated-on-a-white_276714-533.jpg?w=740" alt="" />
+            </motion.div>
+: null }
+</motion.section>
+<motion.section
+initial="hiddenMoon"
+whileInView="visibleMoon"
+>
+{show? null:
+    <motion.section>
+            <motion.img variants={moonAnimation} className={styles.Moon} src="https://hi-news.ru/wp-content/uploads/2013/08/wallpaper-111288-750x469.jpg" alt=""/>
+            </motion.section> 
+}
+</motion.section>
             <img className={styles.Cloud} src="https://img.freepik.com/premium-photo/a-white-cloud-with-a-white-background-and-the-word-cloud-on-it_771335-59254.jpg?w=996" alt="" />
             <img className={styles.Cloud} src="https://img.freepik.com/premium-photo/a-white-cloud-with-a-white-background-and-the-word-cloud-on-it_771335-59254.jpg?w=996" alt="" />
             <img className={styles.Cloud} src="https://img.freepik.com/premium-photo/a-white-cloud-with-a-white-background-and-the-word-cloud-on-it_771335-59254.jpg?w=996" alt="" />
